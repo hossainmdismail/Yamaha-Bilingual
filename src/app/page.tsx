9f3e70ce37f29f7adb1dc1a774880a3898b5cc27
Esmail Khalifa <hossainmdismail9010@gmail.com>
2026-06-04 22:10:40 +0600

    Prevent URL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { redirect } from 'next/navigation';
 import HomeClient from './HomeClient';
 import { getBooleanAppSetting } from '@/lib/server/app-settings';
 
+export const dynamic = 'force-dynamic';
+
 export default async function HomePage() {
   const campaignCompleted = await getBooleanAppSetting('campaign_completed');
 
